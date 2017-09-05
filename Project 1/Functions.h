23645b6a5c9637ea7a4300964b115f78e3cb608d
rookj <rookj@192.168.205.18>
2017-09-05 09:14:50 -0500

    Commit.

---
+++ Project 1/Functions.h
@@ -0,0 +1,19 @@
+/*
+ * Functions.h
+ * Jacob Rook
+ * Data Structures and Algorithms II
+ * Project 1
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include<stdio.h>
+#include<string.h>
+
+
+
+
+
+
+#endif /* FUNCTIONS_H_ */
---
+++ Project 1/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * Jacob Rook
+ * Data Structures and Algorithms II
+ * Project 1
+ */
+
+#include"Functions.h"
+#include "Structs.h"
+
+
+
+int main(void)
+{
+	printf("Hello");
+
+	return 0;
+
+}
+
