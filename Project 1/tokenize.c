2c73aff900db3f90a61c066ddd003f6d5c5f24bb
rookj <rookj@Jacobs-Computer>
2017-09-12 10:35:35 -0500

    Added FindNextToken function

--- Project 1/tokenize.c
+++ Project 1/tokenize.c
@@ -8,45 +8,54 @@
 #include "tokenize.h"
 #include <string.h>
 
-void Tokenizer (char *String, char *Prt[], char TokenChar)
+int Tokenizer (char *String, char *Prt[], char TokenChar)
 {
 	int i;
 	int StringLen = strlen(String);
-	int PlaceHolder=0;	//Variable to hold the index of the first character of a token
-	int PtrCount=0;		//Variable to step through the pointer list
+	int PlaceHolder=0;								//Variable to hold the index of the first character of a token
+	int PtrCount=0;									//Variable to step through the pointer list
 
 	if (StringLen == 0)
-		return;
+		return 0;
+
 	/*
-	for(i=0;i<StringLen;i++)
-	{/*This for loop is to take care of the case that the first couple of characters are
-		the tokenizing character. The loop will move the PlaceHolder to the first non-tokenizing
-		character.
-	///
-		if (String[PlaceHolder] == TokenChar)
-			PlaceHolder++;
-		else
-		{
-			*Prt[PtrCount] = String[PlaceHolder];
-			PtrCount++;
-			break;
-		}
-	}
-	*/
-	/*	This for loop is to take care of the case that the first couple of characters are
-		the tokenizing character. The loop will move the PlaceHolder to the first non-tokenizing
-		character.
-	*/
+	 * This for loop is to take care of the case that the first couple of characters are
+	 *	the tokenizing character. The loop will move the PlaceHolder to the first non-tokenizing
+	 *	character.
+	 */
 	while(( String[PlaceHolder] == TokenChar) && ( PlaceHolder < StringLen ))
 	{
 		PlaceHolder++;
 	}
 
-	while(i<=StringLen)//Reuse the i from the for loop, pick up from where the for loop left off
-	{
+	Prt[PtrCount] = String[PlaceHolder];			//Set the first token pointer to the index of the first token
+	PtrCount++;
+	i=PlaceHolder;
 
+	while(i<StringLen)
+	{
+		if ( String[i] == TokenChar )
+		{
+			String[i] = "\0";						//Set the Token Character to NULL
+
+		}
 
 	}
 
+}
+
+int FindNextToken (char *String, int CurrentLocation, char TokenChar)
+{
+	int StringLen = strlen(String);
+
+	while((String[CurrentLocation] == TokenChar ) && (CurrentLocation < StringLen))
+	{
+		CurrentLocation++;
+	}
+
+	if (CurrentLocation == StringLen)
+		return -1;
+	else
+		return CurrentLocation;
 }
 
--- Project 1/tokenize.h
+++ Project 1/tokenize.h
@@ -18,10 +18,24 @@
  *
  * @param	TokenChar	Character to identify the end of a token
  *
- * @return	Void
+ * @return	int 		Returns the number of tokens
  *
  */
 void Tokenizer (char *String, char *Ptr[], char TokenChar);
+
+/*
+ * @brief	FindNextToken	This function finds the location of the next token.
+ *
+ * @param	*String  		Original string of characters to be tokenized
+ *
+ * @param	CurrentLocation Current location in the string
+ *
+ * @param	TokenChar		Character to identify the end of a token
+ *
+ * @return	int 			Returns the index of the next token if -1, no token found
+ *
+ */
+int FindNextToken (char *String, int CurrentLocation, char TokenChar);
 
 
 #endif /* TOKENIZE_H_ */
