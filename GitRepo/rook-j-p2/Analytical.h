917434c1ca68d216b5fcc53adc833a7b909d2f4b
rookj <jpr28@students.uwf.edu>
2017-10-08 15:45:09 -0500

    In the middle of adding main data structures and variables.
    Added the PQ array. Size is PQMAXSIZE + 1 to count for the 0th element
    not being used.
    Added the Customer_t type structure and the comments to explain the
    variables.
    Added the n, lambda, mu, and M variables all "globally" defined in
    Analytical.h.
    Added the set function prototypes for all the variables above.

--- GitRepo/rook-j-p2/Analytical.h
+++ GitRepo/rook-j-p2/Analytical.h
@@ -10,6 +10,51 @@
 #ifndef ANALYTICAL_H_
 #define ANALYTICAL_H_
 
+#include<stdio.h>
+
+#define PQMAXSIZE 200
+
+float PQ[PQMAXSIZE+1];
+
+/*
+ * @brief	Customer_t		Customer structure to hold the various info needed to
+ * be stored for the customer
+ *
+ * @var		type			Variable to indicate whether the customer is an arrival
+ * or a departure
+ *
+ * @var		arrivalTime		Variable to store the time that the customer arrived
+ *
+ * @var		startOfServiceTime	Time in which the customer begins the service
+ *
+ * @var		departureTime	Variable to store the time when the customer will leave
+ *
+ * @var		nextCust		Variable for the FIFO Queue, this variable will point
+ * to the next customer in line after the current customer
+ */
+typedef struct Cust
+{
+	char 	type;
+	float 	arrivalTime;
+	float	startOfServiceTime;
+	float 	departureTime;
+	struct Cust *nextCust; //For FIFO Queue
+}Customer_t;
+
+
+int 	numArrivals;
+float 	lambda;
+float	mu;
+int		numService;
+
+
+void setN(int N);
+
+void setLambda(float L);
+
+void setMu(float M);
+
+void setM(float M);
 
 
 #endif /* ANALYTICAL_H_ */
