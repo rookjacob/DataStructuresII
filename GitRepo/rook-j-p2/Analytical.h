d9b5fae9613a83dc9873be7361b9cbf713884d4f
rookj <jpr28@students.uwf.edu>
2017-10-09 15:20:58 -0500

    Added prototpyes for PQ and function descriptions for set functions.
    Also added function sections, section for PQ functions, set functions,
    expected calculations section. Need to add a section for the FIFO Queue
    functions.
    
    Functions for PQ: PqEnque, PQDeque, isPQEmpty, percolateUp,
    percolateDown. 
    PQEnque will add a Customer to PQ using percolateUp. 
    PQDeque will take the customer off of the PQ using percoalteDown.

--- GitRepo/rook-j-p2/Analytical.h
+++ GitRepo/rook-j-p2/Analytical.h
@@ -14,7 +14,9 @@
 
 #define PQMAXSIZE 200
 
-float PQ[PQMAXSIZE+1];
+Customer_t *PQ[PQMAXSIZE+1];
+int PQSize;					//Current size of the PQ
+
 
 /*
  * @brief	Customer_t		Customer structure to hold the various info needed to
@@ -48,13 +50,78 @@ float	mu;
 int		numService;
 
 
+//PQ FUNCTIONS
+/*
+ *
+ */
+void PQEnque(Customer_t *Customer);
+
+/*
+ *
+ */
+Customer_t *PQDeque(void);
+
+/*
+ *
+ */
+int isPQEmpty(void);
+
+/*
+ *
+ */
+void percolateUp(Customer_t *Customer);
+
+/*
+ *
+ */
+void percolateDown(int index);
+//END OF PQ FUNCTIONS
+
+
+
+//SET FUNCTIONS
+/*
+ * @brief	setN		This function sets the value for numArrivals to the passed
+ * value
+ *
+ * @param	N			Value to set numArrivals
+ */
 void setN(int N);
 
+/*
+ * @brief	setLambda	This function sets the value for lambda to the passed value
+ *
+ * @param	L			Value to set lambda
+ */
 void setLambda(float L);
 
+/*
+ * @brief	setMu		This function sets the value for mu to the passed value
+ *
+ * @param	M			Value to set mu
+ */
 void setMu(float M);
 
+/*
+ * @brief	setM		This function sets the value for numService to the passed
+ * value
+ *
+ * @param	M			Value to set numService
+ */
 void setM(float M);
+//END OF SET FUNCTIONS
+
+
+
+
+//EXPECTED CALCULATIONS FOR THE ANALYTICAL MODEL
+
+
+//END OF EXPECTED CALCULATIONS FOR THE ANALYTICAL MODEL
+
+
+
+
 
 
 #endif /* ANALYTICAL_H_ */
