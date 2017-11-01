de25f2495b937aab669504360c087898d661bade
rookj <jpr28@students.uwf.edu>
2017-11-01 17:50:05 -0500

    Added prompts to get information. May want to add input validation.

--- rook-j-p3/rook-j-p3.c
+++ rook-j-p3/rook-j-p3.c
@@ -9,11 +9,30 @@
 
 
 #include<stdio.h>
+#include<stdlib.h>
 #include"TravelingSalesman.h"
+#include"RookFunctions.h"
 
 
 int main(void)
 {
+	int numCities = 0;
+	int numTours = 0;
+	int numGen = 0;
+	double percentMut = 0;
+	char newGen;
+	printf("Traveling Salesman Program\n\n");
+
+	printf("Please enter the number of cities to run.\n ");
+	scanf("%d", &numCities);
+	printf("Please enter the number of individual tours are in a given generation.\n");
+	scanf("%d", &numTours);
+	printf("Please enter the number of generations to run.\n");
+	scanf("%d", &numGen);
+	printf("Please enter the percentage of generations that should be comprised of mutations.\n");
+	scanf("%lf", &percentMut);
+	printf("Will a new cityWeights.txt be generated? (y/n)\n");
+	scanf("%c", &newGen);
 
 
 
