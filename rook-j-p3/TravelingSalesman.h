280fd3805355c522e2e70b4770435e6d5ae05bd6
rookj <jpr28@students.uwf.edu>
2017-11-01 17:56:42 -0500

    Added couple of main function prototypes still thinking through
    Functions added:
    startTravel - will ignitiate the Traveling salesman program. Will call
    the intitTourVar function to initialize the global variables,
    genNewWeight if a new batch of weights are wanted, call populateGraph to
    do just that, then execBruteForce then execGenetic.

--- rook-j-p3/TravelingSalesman.h
+++ rook-j-p3/TravelingSalesman.h
@@ -10,6 +10,57 @@
 #ifndef TRAVELINGSALESMAN_H_
 #define TRAVELINGSALESMAN_H_
 
+#define MAXCITIES 20
+
+double CITYGRAPH[MAXCITIES][MAXCITIES];
+double PERMUTATION[MAXCITIES];
+
+typedef struct Gen {
+	int cityTour[MAXCITIES];
+	double tourWeight;
+}Generation;
+
+
+/*
+ *
+ */
+void startTravel(int numCities, int numTours, int numGen, double percentMut);
+
+/*
+ *
+ */
+void initTourVar(int numCities, int numTours, int numGen, double percentMut);
+
+/*
+ *
+ */
+void genNewWeight(void);
+
+/*
+ *
+ */
+void populateGraph(void);
+
+/*
+ *
+ */
+void execBruteForce(void);
+
+/*
+ *
+ */
+int fact(int n);
+
+/*
+ *
+ */
+void swap(int p, int q);
+
+/*
+ *
+ */
+void execGenetic(void);
+
 
 
 #endif /* TRAVELINGSALESMAN_H_ */
