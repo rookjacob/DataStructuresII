f6d2fb39292df219290c45af172f455bf7b8ec09
rookj <jpr28@students.uwf.edu>
2017-11-14 10:05:02 -0600

    Added user interface to rook-j-p4

--- rook-j-p4/rook-j-p4.c
+++ rook-j-p4/rook-j-p4.c
@@ -11,8 +11,37 @@
 
 int main(void)
 {
+	char cond;
+	printf("Would you like to convert SimParameters.txt to a binary file? (y/n)");
+	scanf("\n%c", &cond);
+	if(cond == 'Y' || cond == 'y')
+	{
+		//Call function
+	}
+	while(1)
+	{
+		printf("\n "
+				" Option		Choice"
+				"    1			Implement a Monte Carlo Simulation\n"
+				"    2          Implement a Simple Monte Carlo Algorithm\n"
+				"	 3 			Exit Program");
+		scanf("\n%c", &cond);
 
-	printf("Would you like to convert ")
+		switch(cond)
+		{
+		case '1':
+			//Call function
+			break;
+		case '2':
+			//Call function
+			break;
+		case '3':
+			return 0;
+		default:
+			printf("Invalid Option\n");
+			break;
+		}
+	}
 
 
 	return 0;
