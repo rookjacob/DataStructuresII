d894513f640841bc88555ad34cb34e5144563d7c
rookj <jpr28@students.uwf.edu>
2017-11-14 08:59:51 -0600

    Added header format, and main function. Need to add file descriptions.

--- rook-j-p4/rook-j-p4.c
+++ rook-j-p4/rook-j-p4.c
@@ -1,8 +1,19 @@
 /*
- * rook-j-p4.c
+ * @file rook-j-p4.c
  *
- *  Created on: Nov 13, 2017
- *      Author: rookj
+ * @author Jacob Rook
+ * @date 11/16/2017
+ * @info Course COP 4534
+ * Project 4
  */
 
+#include<stdio.h>
 
+int main(void)
+{
+
+
+
+
+	return 0;
+}
