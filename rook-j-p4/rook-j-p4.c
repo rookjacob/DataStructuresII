53e0247129e2783ae1c96d063bd5dd536e0ea07f
rookj <jpr28@students.uwf.edu>
2017-11-14 13:39:25 -0600

    print formating

--- rook-j-p4/rook-j-p4.c
+++ rook-j-p4/rook-j-p4.c
@@ -23,11 +23,14 @@ int main(void)
 	}
 	while(1)
 	{
-		printf("\n "
-				" Option		Choice"
+		printf("\n"
+				" Option		Choice\n"
 				"    1			Implement a Monte Carlo Simulation\n"
 				"    2          Implement a Simple Monte Carlo Algorithm\n"
-				"	 3 			Exit Program");
+				"	 3 			Exit Program\n"
+				"\n"
+				"Please enter an Option: ");
+
 		scanf("\n%c", &cond);
 
 		switch(cond)
