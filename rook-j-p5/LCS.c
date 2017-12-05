92eb57f7c32327deb3e9448aed703863a21133e0
rookj <jpr28@students.uwf.edu>
2017-12-04 20:08:17 -0600

    Added main c files

---
+++ rook-j-p5/LCS.c
@@ -0,0 +1,8 @@
+/*
+ * LCS.c
+ *
+ *  Created on: Dec 4, 2017
+ *      Author: rookj
+ */
+
+
---
+++ rook-j-p5/LCS.h
@@ -0,0 +1,13 @@
+/*
+ * LCS.h
+ *
+ *  Created on: Dec 4, 2017
+ *      Author: rookj
+ */
+
+#ifndef LCS_H_
+#define LCS_H_
+
+
+
+#endif /* LCS_H_ */
