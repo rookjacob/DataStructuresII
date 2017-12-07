2a9484362d461babc29cae207cdfbbc018e9ad9e
rookj <jpr28@students.uwf.edu>
2017-12-07 17:20:53 -0600

    string2[string2_Length - 1] = '\0';

--- rook-j-p5/LCS.c
+++ rook-j-p5/LCS.c
@@ -80,9 +80,9 @@ void init_XY_C(char *FileName)
 	fclose(fp);
 	int string1_Length = strlen(string1);
 	int string2_Length = strlen(string2);
-	string1[string1_Length] = '\0';
+	string1[string1_Length - 1] = '\0';
 	printf("[%s]\n",string1);
-	string2[string2_Length] = '\0';
+	string2[string2_Length - 1] = '\0';
 	printf("[%s]\n", string2);
 
 	if(string1_Length < string2_Length)
