bcad2540af8ddba9b61cbb12a5718099d4e079f8
rookj <jpr28@students.uwf.edu>
2017-12-07 00:11:27 -0600

    Print statment

--- rook-j-p5/LCS.c
+++ rook-j-p5/LCS.c
@@ -198,9 +198,10 @@ void init_Interval_List(char *FileName)
 	INT_LIST[0] = strlen(tmp);
 	for(i = 1; i <= NUM_LCS; i++)
 	{
-		if(fgets(tmp, MAX_LCS_LENGTH + 2, fp))
+		if(fgets(tmp, MAX_LCS_LENGTH + 1, fp))
 		{
 			INT_LIST[i] = strlen(tmp) + INT_LIST[i - 1];
+			printf("%d %d\n", i, INT_LIST[i]);
 			if(!INT_LIST[i])
 			{
 				printf("Error reading string %d", i);
